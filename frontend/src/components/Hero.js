2213ea9da05a8671fe47a7c64fd82cc140ac190b
Armon <abarakchi@ucsd.edu>
2026-03-20 18:33:45 -0700

    fixed build

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       <section className="hero">
           <div className="hero-text">
               <div className="tag">
-                  <h2>Electrical<br/>Engineer.</h2>
+                  <h2>Software<br/>Developer.</h2>
               </div>
               <p className="blurb">
                   Trained in electrical engineering. Focused on using my cross-disciplinary background to create elegant
